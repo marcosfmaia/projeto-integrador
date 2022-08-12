c7e6873b88feae2464fe64afdef3cbef23f0aa73
MarcosF <marcosfelipemaia2001@gmail.com>
2022-08-11 23:09:39 -0300

    first commit

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import LoginMain from '../../components/LoginMain'; 
+
+
+export default function Login () {
+    return (
+        <container  className="container">
+            <LoginMain />
+        </container>
+    )
+}
