98e04dd187bfc2fff48bf28f733f0b374dadfb49
geneselidias <geneselidias@gmail.com>
2022-08-12 16:42:23 -0300

    arquivo index.jsx do cadastro adicionado e outros arquivos modificados

---
+++ book-lovers-geneseli/src/components/CadastroMain/index.jsx
@@ -0,0 +1,43 @@
+import './style.css'
+
+export default function CadastroMain ( { children} ) {
+    return (
+        <div>
+            {children}
+
+            <div className="group-title__cadastro">
+                <h2 className="title__registrer">Não tenho cadastro</h2>
+            </div>
+
+            <form action="" className="form-group">
+                <fieldset className="form-group__cadastro">
+                    <label htmlFor="user-nome" className="label__text">Nome</label>
+                    <i class="bi bi-envelope-fill login__icon"></i>
+                    <input type="nome" id="user-nome" className="user-cadastro" placeholder="Nome Completo" />
+                </fieldset>
+
+                <fieldset className="form-group__cadastro">
+                    <label htmlFor="user-email" className="label__text">Email</label>
+                    <i class="bi bi-envelope-fill login__icon"></i>
+                    <input type="email" id="user-email" className="user-cadastro" placeholder="Digite seu email" />
+                </fieldset>
+
+                <fieldset className="form-group__cadastro">
+                    <label htmlFor="user-telefone" className="label__text">Telefone</label>
+                    <i class="bi bi-lock-fill login__icon"></i>
+                    <input type="number" name="user-telefone"  placeholder="Digite seu telefone"/>
+                </fieldset>
+
+                <fieldset className="form-group__login">
+                    <label htmlFor="password" className="label__text">Senha</label>
+                    <i class="bi bi-lock-fill login__icon"></i>
+                    <input type="password" name="pass" id="password" className="user-login" placeholder="Digite uma senha forte"/>
+                </fieldset>
+
+        
+
+                <button className="btn-cadastro">Cadastrar</button>
+            </form>
+        </div>
+    )
+}
--- book-lovers-geneseli/src/pages/Home/index.jsx
+++ book-lovers-geneseli/src/pages/Home/index.jsx
@@ -1,11 +1,10 @@
 import Container from "../../components/Container";
-import Header from "../../components/Header";
 
 function Home () {
     return (
         <div>
             <Container />
-            <Header />
+            
         </div>
 
     );
