249efd1c40adb88febd6b26ea97dc71d858511e9
geneselidias <geneselidias@gmail.com>
2022-08-12 17:02:31 -0300

    arquivos index.jsx e style.css da página de cadastro adicionados

---
+++ book-lovers-geneseli/src/pages/Cadastro/index.jsx
@@ -0,0 +1,11 @@
+import './style.css'
+import CadastroMain from '../../components/CadastroMain'; 
+
+
+export default function Login () {
+    return (
+        <container  className="container">
+            <CadastroMain />
+        </container>
+    );
+}
