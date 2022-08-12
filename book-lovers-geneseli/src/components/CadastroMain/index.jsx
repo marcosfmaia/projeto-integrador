cb4d3ac1872c65f35c412a4db59d7ae9ef3e6164
geneselidias <geneselidias@gmail.com>
2022-08-12 17:21:46 -0300

    arquivos index.jsx e style.css do cadastro main modificados

--- book-lovers-geneseli/src/components/CadastroMain/index.jsx
+++ book-lovers-geneseli/src/components/CadastroMain/index.jsx
@@ -13,27 +13,25 @@ export default function CadastroMain ( { children} ) {
                 <fieldset className="form-group__cadastro">
                     <label htmlFor="user-nome" className="label__text">Nome</label>
                     
-                    <input type="nome" id="user-nome" className="user-cadastro" placeholder="Nome Completo" />
+                    <input type="nome" id="user-nome" className="user-nome" placeholder="Nome Completo" />
                 </fieldset>
 
                 <fieldset className="form-group__cadastro">
                     <label htmlFor="user-email" className="label__text">Email</label>
                    
-                    <input type="email" id="user-email" className="user-cadastro" placeholder="Digite seu email" />
+                    <input type="email" id="user-email" className="user-email" placeholder="Digite seu email" />
                 </fieldset>
 
                 <fieldset className="form-group__cadastro">
                     <label htmlFor="user-telefone" className="label__text">Telefone</label>
                     
-                    <input type="number" name="user-telefone"  placeholder="Digite seu telefone"/>
+                    <input type="number" name="user-telefone" className='user-telefone' placeholder="Digite seu telefone"/>
                 </fieldset>
 
-                <fieldset className="form-group__login">
+                <fieldset className="form-group__cadastro">
                     <label htmlFor="password" className="label__text">Senha</label>
-                    <input type="password" name="pass" id="password" className="user-login" placeholder="Digite uma senha forte"/>
+                    <input type="password" name="pass" id="password" className="user-senha" placeholder="Digite uma senha forte"/>
                 </fieldset>
-
-        
 
                 <button className="btn-cadastro">Cadastrar</button>
             </form>
