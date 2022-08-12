0c3557864b852047add5e0b5a423e856cbcec34e
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-12 11:49:29 -0300

    chamando componente Menu

--- pi-alphatech/src/App.js
+++ pi-alphatech/src/App.js
@@ -4,7 +4,8 @@ import Menu from './components/Menu';
 function App() {
   return (
     <div className="App">
-      
+    
+    <Menu/>
     </div>
   );
 }
--- pi-alphatech/src/components/Menu.js
+++ pi-alphatech/src/components/Menu.js
@@ -2,7 +2,7 @@ function Menu() {
 
     return (
         <div>
-            
+            <h1>Menu</h1>
         </div>
     )
 
