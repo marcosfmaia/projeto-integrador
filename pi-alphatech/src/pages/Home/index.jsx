ac25061f0bae473c32550ba1f38306b5420e95b0
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-13 07:25:05 -0300

    criando a Home

---
+++ pi-alphatech/src/pages/Home/index.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import './styles.css';
+
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Hours from "../../components/Hours";
+import Main from "../../components/Main";
+
+export default function Home() {
+
+    const [show, setShow] = React.useState(true)
+
+
+    return (
+        <Container>
+            <Header />
+            <Main>
+                <Hours show={show} />
+                <button className="btn" onClick={() => setShow(!show)}>Mostrar/Esconder</button>
+            </Main>
+            <Footer />
+        </Container>
+    );
+}
