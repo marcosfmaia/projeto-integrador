14850f29f253053dcd158a4ca22101ec799c1941
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-12 21:06:06 -0300

    limpando arquivo app.js

--- pi-alphatech/src/App.js
+++ pi-alphatech/src/App.js
@@ -1,9 +1,4 @@
 import './App.css';
-import Busca from './components/HomePage/Busca/Busca';
-import Login from './components/HomePage/Login/Login';
-import Cadastro from './components/HomePage/Cadastro/Cadastro';
-import Menu from './components/HomePage/Menu/Menu';
-import Card from './components/HomePage/Card/Card';
 
 function App() {
   return (
