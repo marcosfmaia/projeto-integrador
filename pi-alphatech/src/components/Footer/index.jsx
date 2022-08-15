1cf114406986a48c35f8a90be34d5af6ce4aa58f
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-15 05:09:05 -0300

    pastas de componentes de pages recriadas

---
+++ pi-alphatech/src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css';
+import logo from '../../assets/logoBookLovers48x48.png';
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <img className='footer__logo' src={logo} alt='Logo do site' />
+
+            <div className='footer__menu'>
+                <a className='menu__item' href='#'>Home</a>
+                <a className='menu__item' href='#'>Sobre</a>
+                <a className='menu__item' href='#'>Contato</a>
+            </div>
+        </footer>
+    );
+}
---
+++ pi-alphatech/src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import './styles.css';
+import logo from '../../assets/logoBookLovers48x48.png';
+
+export default function Header() {
+    return (
+        <header className='header'>
+            <img className='header__logo' src={logo} alt='Logo do site' />
+
+            <nav className='header__menu'>
+                <a className='menu__item' href='#'>Novidade</a>
+                <a className='menu__item' href='#'>Mais solicitados</a>
+                <a className='menu__item' href='#'>Contato</a>
+            </nav >
+        </header >
+    );
+}
