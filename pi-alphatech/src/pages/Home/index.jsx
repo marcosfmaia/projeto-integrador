6287b8b1e49da0bddfc79e1115901e89f15a203a
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-15 04:12:50 -0300

    limpando arquivos para refazer

--- pi-alphatech/src/pages/Home/index.jsx
+++ pi-alphatech/src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
     return (
         <Container>
-            <Header />
+            <Header/>
             <Main/>
             <Footer />
         </Container>
