f77cfc2544c3b4fc44fa8ad2c18ae71f6c07c21d
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-15 06:00:37 -0300

    alteração do footer e do seu css e css do header

--- pi-alphatech/src/components/Footer/index.jsx
+++ pi-alphatech/src/components/Footer/index.jsx
@@ -4,12 +4,9 @@ import logo from '../../assets/logoBookLovers48x48.png';
 export default function Footer() {
     return (
         <footer className='footer'>
-            <img className='footer__logo' src={logo} alt='Logo do site' />
 
             <div className='footer__menu'>
-                <a className='menu__item' href='#'>Novidades</a>
-                <a className='menu__item' href='#'>Mais solicitados</a>
-                <a className='menu__item' href='#'>Contato</a>
+                <img className='footer__logo' src={logo} alt='Logo do site' />
             </div>
         </footer>
     );
