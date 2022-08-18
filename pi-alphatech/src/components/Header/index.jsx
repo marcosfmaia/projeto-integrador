1b2dd9c2d416e7b6e18a4a17408651550630e65f
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-18 10:47:03 -0300

    inserção da barra de busca no header da página Home

--- pi-alphatech/src/components/Header/index.jsx
+++ pi-alphatech/src/components/Header/index.jsx
@@ -6,9 +6,14 @@ export default function Header() {
         <header className='header'>
             <img className='header__logo' src={logo} alt='Logo do site' />
 
+            <div>            
+            <input placeholder='Digite o que você procura' type='text' className='base__input' value=''></input>
+            <button type='submit'className='search'>Buscar</button>
+            </div>
+            
             <nav className='header__menu'>
                 <a className='menu__item' href='#'>Login</a>
-                <button className='button_signup'><a>Cadastre-se</a></button>
+                <button className='button__signup'><a>Cadastre-se</a></button>
                 <a className='menu__item' href='#'>Ajuda</a>
             </nav >
         </header >
