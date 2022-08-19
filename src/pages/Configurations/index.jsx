baa291036e1afb8fee73425cc50377f610ab40aa
geneselidias <geneselidias@gmail.com>
2022-08-19 11:15:54 -0300

    arquivo index.jsx criado, imagem adicionada e arquivo routes modificado

---
+++ src/pages/Configurations/index.jsx
@@ -0,0 +1,35 @@
+import './style.css'
+
+export default function Configurations ( { children} ) {
+    return (
+        <div className='container'>
+            {children}
+
+            <h1 className='title__configurations'>Configurações da Conta</h1>
+            <hr className='line'></hr>
+
+            <h2 className='title__account'>Conta</h2>
+            <img className='title__account__image' src='./assets/profile.png' />
+
+            <div className='configurations__main'>
+                <h3 className='password'>Senha</h3>
+                <button className='password__btn'>Alterar</button>
+                <hr className='line__password'></hr>
+
+                <h4 className='email'>Endereço de e-mail</h4>
+                <p className='email__text'>Seu endereço de e-mail é digitalhouse@gmail.com</p>
+                <button className='email__btn'>Alterar</button>
+                <hr className='line__email'></hr>
+
+                <h5 className='disable__account'>Desativar Conta</h5>
+                <p className='disable__account__text'>Deseja desativar sua conta?</p>
+                <button className='disable__account__btn'>Desativar</button>
+                
+            </div>
+
+            <div className='footer'>© BookLovers ©</div>
+        </div>
+
+
+    )
+}
