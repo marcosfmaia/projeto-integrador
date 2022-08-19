7c97d3b7f48ce8548a72191f36d3a067e09b3952
geneselidias <geneselidias@gmail.com>
2022-08-19 16:15:49 -0300

    modified index.jsx structure and added css structure

--- src/pages/Configurations/index.jsx
+++ src/pages/Configurations/index.jsx
@@ -5,32 +5,30 @@ export default function Configurations ( { children} ) {
         <div className='container'>
             {children}
 
-            <div className='configurations__header'>
-                <h1 className='title__configurations'>Configurações da Conta</h1>
-                <hr className='line'></hr>
+            <div className='box'>            
 
-    
-                <h2 className='title__account'>Conta</h2>
-                <img className='title__account__image' src='./assets/profile.png' />
-            </div>
+                <div className='configurations__header'>
+                    <h1 className='title__configurations'>Configurações da Conta</h1>            
+                </div>
 
-            <div className='configurations__main'>
-                <h3 className='password'>Senha</h3>
-                <button className='password__btn'>Alterar</button>
-                <hr className='line__password'></hr>
+                <div className='configurations__main'>
+                    <h3 className='password'>Senha</h3>
+                    <button className='change__btn'>Alterar</button>
+                    <hr className='line'></hr>
 
-                <h4 className='email'>Endereço de e-mail</h4>
-                <p className='email__text'>Seu endereço de e-mail é digitalhouse@gmail.com</p>
-                <button className='email__btn'>Alterar</button>
-                <hr className='line__email'></hr>
+                    <h3 className='email'>Endereço de e-mail</h3>
+                    <p className='main__text'>Seu endereço de e-mail é <strong>digitalhouse@gmail.com</strong></p>
+                    <button className='change__btn'>Alterar</button>
+                    <hr className='line'></hr>
 
-                <h5 className='disable__account'>Desativar Conta</h5>
-                <p className='disable__account__text'>Deseja desativar sua conta?</p>
-                <button className='disable__account__btn'>Desativar</button>
+                    <h3 className='disable__account'>Desativar Conta</h3>
+                    <p className='main__text'>Deseja desativar sua conta?</p>
+                    <button className='disable__account__btn'>Desativar</button>
                 
-            </div>
+                </div>
 
-            <div className='configurations__footer'>© BookLovers ©</div>
+                <div className='configurations__footer'>© BookLovers ©</div>
+            </div>
         </div>
 
 
