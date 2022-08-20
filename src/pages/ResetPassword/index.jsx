4d56d72c85793eacbda3728f98a8d840bd0ee7d5
MarcosF <marcosfelipemaia2001@gmail.com>
2022-08-19 23:57:52 -0300

    reset password

--- src/pages/ResetPassword/index.jsx
+++ src/pages/ResetPassword/index.jsx
@@ -3,14 +3,20 @@ import './style.css'
 import * as React from "react";
 import { Link } from 'react-router-dom'
 
-export default function ResetPassword () {
+export default function ResetPassword () {    
     return (
         <div className='container'>
-            <form className='form-reset'>
+            <form className='form-reset' onSubmit=''>
                 <h2 className='title__reset'>Recuperar senha</h2>
                 <fieldset className='form-group__reset'>
                     <i class="bi bi-lock-fill email__icon"></i>
-                    <input type="email" className='reset__pass' placeholder='Digite sua nova senha'/>
+                    <input 
+                    type="email" 
+                    className='reset__pass' 
+                    placeholder='Digite sua nova senha'
+                    // name='password'
+                    // value={form.password
+                    />
                 </fieldset>
 
                 <button className="btn-reset">RECUPERAR</button>
