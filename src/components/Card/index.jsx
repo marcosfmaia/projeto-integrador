bedc6145247944946b1f84e372a5eb5d969b0c46
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-24 15:42:01 -0300

    estilizando imagens dos livros

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,12 @@
 import './styles.css'
+import livro from '../../assets/livro01.webp';
 
 export default function Card() {
     return (
         <div className="card">
         <div className="card__body">
             
+        <img className='img__livro' src={livro} alt='Livro' />
             <h5 className="card__title">Título do livro</h5>
             <h4 className="card__subtitle">Gênero/Área de Conhecimento</h4><br></br>
             <h3>Descrição</h3>
