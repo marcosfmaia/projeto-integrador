5907a4c221d97e7b14e1afa7130bf53138385b07
geneselidias <geneselidias@gmail.com>
2022-08-24 15:37:21 -0300

    page structure change email finished

---
+++ src/pages/ChangeEmail/index.jsx
@@ -0,0 +1,42 @@
+import './style.css'
+import { Link } from 'react-router-dom'
+import Container from '../../components/Container';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+export default function ChangeEmail () {
+    return (
+        <Container>
+            <Header />
+            <form className='form-change-email' onSubmit=''>
+                <h2 className='title-change-email'>Alterar E-mail</h2>
+
+                <fieldset className="form-group__reset">
+                    <i class="bi bi-envelope-fill reset__icon"></i>
+                    <input 
+                    type="email" 
+                    className='reset__pass' 
+                    placeholder='Digite seu email atual' 
+                    />
+                </fieldset>
+
+                <fieldset className="form-group__reset">
+                    <i class="bi bi-envelope-fill reset__icon"></i>
+                    <input 
+                    type="email" 
+                    className='reset__pass' 
+                    placeholder='Digite um novo email' 
+                    />
+                </fieldset>
+
+                <button className="btn-change-email">Alterar</button>
+
+                <div className="change-group__previus">
+                    <i class="bi bi-arrow-left-short previus__arrow"></i>
+                    <Link to="/configurations" className="previus__configurations">Voltar</Link>
+                </div>
+            </form>
+            <Footer />
+        </Container>
+    )
+}
