1c372fc7181a3665825c04b8573667607c47fb57
geneselidias <geneselidias@gmail.com>
2022-08-30 20:46:33 -0300

    resolving settings page conflict

--- src/pages/Configurations/index.jsx
+++ src/pages/Configurations/index.jsx
@@ -1,4 +1,7 @@
-import './style.css'
+import "./style.css"
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { Link } from "react-router-dom";
 
 
 export default function Configurations ({children}) {
@@ -6,35 +9,6 @@ export default function Configurations ({children}) {
 
     return (
 
-        <div className='container'>
-            {children}
-
-            <div className='box'>            
-
-                <div className='configurations__header'>
-                    <h1 className='title__configurations'>Configurações da Conta</h1>            
-                </div>
-
-                <div className='configurations__main'>
-                    <h3 className='password'>Senha</h3>
-                    <button className='change__btn'>Alterar</button>
-                    <hr className='line'></hr>
-
-                    <h3 className='email'>Endereço de e-mail</h3>
-                    <p className='main__text'>Seu endereço de e-mail é <strong>digitalhouse@gmail.com</strong></p>
-                    <button className='change__btn'>Alterar</button>
-                    <hr className='line'></hr>
-
-                    <h3 className='disable__account'>Desativar Conta</h3>
-                    <p className='main__text'>Deseja desativar sua conta?</p>
-                    <button className='disable__account__btn'>Desativar</button>
-                
-                </div>
-
-                <div className='configurations__footer'>© BookLovers ©</div>
-            </div>
-        
-        
         <div className='container'>
             {children}
         
@@ -70,6 +44,6 @@ export default function Configurations ({children}) {
             <Footer />
     
         </div>
-        </div>
+        
     )
 }
