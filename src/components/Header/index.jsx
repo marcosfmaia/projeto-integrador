f204a15255565a77fdd432b802d4da91da18ad17
Ananda Fidelis <ananda.fidelis@gmail.com>
2022-08-30 11:15:42 -0300

    substituindo o Card, Footer, Header e ModalDetails

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,37 @@
 import './styles.css';
 import logo from '../../assets/logoBookLovers48x48.png';
+import iconUser from '../../assets/profile.png';
+import React from 'react';
 
 export default function Header() {
+    const [filter, setFilter] = React.useState('');
+
+    const handleChange = (e) => {
+        setFilter(e.target.value);
+    }
+
     return (
         <header className='header'>
-            <img className='header__logo' src={logo} alt='Logo do site' />
+            <img className='header__logo' src={logo} alt='Book Lovers'/>
 
-            <div>            
-            <input placeholder='Digite o que você procura' type='text' className='base__input' value=''></input>
+            <form className='header__form'>            
+            <input
+
+                className='header__input'
+                placeholder='Digite o que você procura'
+                type='text'
+                name='filter'
+                value={filter}
+                onChange={handleChange}
+            />
             <button type='submit'className='search'>Buscar</button>
-            </div>
+            </form>
             
             <nav className='header__menu'>
-                <a className='menu__item' href='#'>Login</a>
-                <button className='button__signup'><a>Cadastre-se</a></button>
-                <a className='menu__item' href='#'>Ajuda</a>
+            <i class="bi bi-person-fill header__user"></i>
+                <a className='menu__item' href='/../Login'>Login</a>
+                <button className='button__signup' href='/../SignUp'><a>Cadastre-se</a></button>
+                <a className='menu__item' href='/../Help'>Ajuda</a>
             </nav >
         </header >
     );
---
+++ src/components/ModalDetails/index.jsx
@@ -0,0 +1,76 @@
+import './styles.css'
+
+import * as React from 'react';
+import axios from 'axios';
+
+export default function ModalDetails(props) {
+
+    /*React.useEffect(() => {
+        const getAdress = async () => {
+            const response = await axios.get('https://viacep.com.br/ws/${props.user.zipcode}/json/');
+        }
+        getAdress();
+    })*/
+    
+    return (
+        <div className='modal'>
+            
+            <div className='modal__body'>
+                <button 
+                    className='modal__btnx'
+                    onClick={() => props.setShowModal(false)}
+                >
+                    X
+                    </button>
+
+                <div>
+                    <h6 className='modal__title'>Título do Livro</h6>
+                    <h5 className='modal__subtitle'>Gênero do livro</h5>
+                    
+                    <div>
+                        <p className='modal__cad'>Cadastro:</p>
+                    </div>
+
+                    <div>
+                        <p className='modal__disp'>Disponibilidade: </p>
+                    </div>
+
+                </div>
+
+                <div className='description'>
+                    <h4 className='text__title'>Descrição</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat tempora ipsa eligendi dolores natus pariatur aperiam magnam facere dolorum? Odio id ut excepturi corrupti eos ab tempore illum ducimus beatae aut, inventore neque, fuga culpa non doloremque repudiandae quas pariatur!</p>
+                
+                    <aside>
+                    <p className='modal__text'>Endereço: <br /></p>
+                    <p className='modal__text'>Contato: </p>
+                    </aside>
+                
+                </div>
+
+
+                
+
+
+                <div>
+                <h3 className='img__title'>Imagens</h3>
+                <ul>
+                    <li className='modal__img'></li>
+                    <li className='modal__img'></li>
+                    <li className='modal__img'></li>
+                </ul>
+                </div>
+
+                <div>
+                    <button 
+                    className='modal__btn'
+                    onClick={() => props.setShowModal(false)}
+                    >
+                    Fechar
+                    </button>
+                </div>
+
+            </div>
+        </div>
+    )
+}
